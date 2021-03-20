db62a6bba36c346ac6a0a59a9a4ab5a1251c1d79
Chibuokem ibezim <chibuokemibezim@gmail.com>
2021-03-20 17:36:58 +0100

    modified wallet library

--- library/wallet/wallet.js
+++ library/wallet/wallet.js
@@ -1,36 +1,31 @@
 const walletService = require('../../services/wallet/wallet');
 class Wallet{
     constructor(secretKey) {
-        this.secretKey = secretKey;
+        this.walletService = new walletService(secretKey);
     }
     //service to get wallet data
     async getWalletData(token, address){
-        const walletServiceClass = new walletService();
-        return await walletServiceClass.getWalletData(token, address);
+        return await this.walletService.getWalletData(token, address);
     }
 
     //generate wallet address
     async generateWalletAddress(token) {
-        const walletServiceClass = new walletService(this.secretKey);
-        return await walletServiceClass.generateWalletAddress(token);
+        return await this.walletService.generateWalletAddress(token);
     }
 
     //get wallet addresses
     async getWalletAddresses(token){
-        const walletServiceClass = new walletService(this.secretKey);
-        return await walletServiceClass.getWalletAddresses(token);
+        return await this.walletService.getWalletAddresses(token);
     }
 
     //delete wallet address
     async deleteWalletAddress(token, address){
-        const walletServiceClass = new walletService(this.secretKey);
-        return walletServiceClass.deleteWalletAddress(token, address);
+        return await this.walletService.deleteWalletAddress(token, address);
     }
 
     //function to send token
     async sendToken(token, amount, wallet, memo = null, tag = null){
-        const walletServiceClass = new walletService(this.secretKey);
-        return walletServiceClass.sendToken(token, amount, wallet, memo, tag);
+        return await this.walletService.sendToken(token, amount, wallet, memo, tag);
     }
 
 }
